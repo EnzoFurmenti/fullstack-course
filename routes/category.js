d650a43713b9e12b98a880bb39e737528a9aa6da
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-14 15:46:44 +0300

    changed  post to patch method for category update

--- routes/category.js
+++ routes/category.js
@@ -6,5 +6,5 @@ router.get('/', controllers.getAll);
 router.get('/:id', controllers.getById);
 router.delete('/:id', controllers.remove);
 router.post('/', controllers.create);
-router.post('/:id', controllers.update);
+router.patch('/:id', controllers.update);
 module.exports = router;
