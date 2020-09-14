35d1c4f4ad3446c6245333a0988848cdfaf04082
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-14 15:41:56 +0300

    add routes and controllers for analytics

---
+++ routes/analytics.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const controllers = require('../controllers/analytics');
+const router = express.Router();
+
+
+router.get('/overview', controllers.overview);
+router.get('/analytics', controllers.analytics);
+
+module.exports = router;
