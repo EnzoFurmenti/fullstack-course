3eccbdb7f2c39df9be3ab7888cd8d7c0b1665c40
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-20 22:02:20 +0300

    add getByCategoryId functions

--- controllers/position.js
+++ controllers/position.js
@@ -1,8 +1,16 @@
 const Position = require('../models/Position');
 const errorHandler = require('../utils/errorHandler');
 
-
-module.exports.create = function(req, res) {
+module.exports.getByCategoryId = async function(req, res) {
+    try{
+        const positions = await Position.find({
+            category:req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(positions);
+    }catch (e){
+        errorHandler(e);
+    }
 
 };
 
