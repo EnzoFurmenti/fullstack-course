3ec27878ef831a6bab6c9274c34f47b0f7deca0f
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-20 22:11:59 +0300

    add getAll category func

--- controllers/category.js
+++ controllers/category.js
@@ -3,6 +3,13 @@ const Position = require('../models/Position');
 const errorHandler = require('../utils/errorHandler');
 
 
+module.exports.getAll = async function(req, res) {
+    try{
+    const categories = await Category.find({user: req.user.id});
+    res.status(200).json(categories);
+    }catch (e){
+        errorHandler(e);
+    }
 };
 
 module.exports.getById = function(req, res) {
