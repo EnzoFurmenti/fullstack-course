72a1088ee8dc10b21042ef1229b8c9ef8a104331
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-20 22:02:48 +0300

    add remove functions

--- controllers/position.js
+++ controllers/position.js
@@ -28,6 +28,15 @@ module.exports.create = async function(req, res) {
     }
 };
 
-module.exports.update = function(req, res) {
+module.exports.remove = async function(req, res) {
+    try{
+    await Position.remove({_id:req.params.id});
+    res.status(200).json({
+        message:'Позиция была удалена'
+    })
+    }catch (e){
+        errorHandler(e);
+    }
+};
 
 };
