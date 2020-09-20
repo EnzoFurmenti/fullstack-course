98788776709d6c9577e6ce499e9ec2bb8b643836
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-20 22:12:30 +0300

    add remove category func

--- controllers/category.js
+++ controllers/category.js
@@ -21,8 +21,16 @@ module.exports.getById = async function(req, res) {
     }
 };
 
-module.exports.remove = function(req, res) {
-
+module.exports.remove = async function(req, res) {
+    try{
+        await Position.remove({_id:req.params.id});
+        await Position.remove({category:req.params.id});
+        res.status(200).json({
+            message:'Категория была удалена'
+        })
+    }catch (e){
+        errorHandler(e);
+    }
 };
 
 module.exports.create = function(req, res) {
