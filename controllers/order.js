ea5c8b3257c3bbbae1c5ebc412ac15f67de77928
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-22 10:13:06 +0300

    init getAll order controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,9 +1,42 @@
 const Order = require('../models/Order');
 const errorHandler = require('../utils/errorHandler');
 
-};
+//(get)localhost:5000/api/order?offset=2&limit=5
+module.exports.getAll = async function(req, res) {
+    const query = {
+        user:req.user.id
+    };
+    //Дата старта
+    if(req.query.start){
+      query.date =  {
+          // Больше или равно
+          $gte: req.query.start
+      };
+    }
 
-module.exports.create = function(req, res) {
+    if(req.query.end){
+        if(!query.date){
+            query.date = {};
+        }
+        query.date['&lte'] =  req.query.end;
+    }
+
+    if(req.query.order){
+        query.order = +req.query.order;
+    }
+
+    try{
+        const orders = await Order
+            .find(query)
+            .sort({date: -1})
+            .skip(+req.query.offset)
+            .limit(+req.query.limit);
+
+        res.status(200).json(orders);
+    }catch(e){
+        errorHandler(res, e);
+    };
+};
 
 module.exports.create = async function(req, res) {
     try{
