c5e13e85f217f8083028f2b95d095e867c5ddf02
EnzoF <EnzoF@MacBook-Pro-2.local>
2020-09-22 09:56:35 +0300

    init create order controller

--- controllers/order.js
+++ controllers/order.js
@@ -5,5 +5,21 @@ const errorHandler = require('../utils/errorHandler');
 
 module.exports.create = function(req, res) {
 
+module.exports.create = async function(req, res) {
+    try{
+        const lastOrder = await Order
+            .findOne({user:req.user.id})
+            .sort({date: -1});
+        
+        const maxOrder = lastOrder ? lastOrder.order : 0;
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order:maxOrder +1
+        }).save();
+        res.status(201).json(order);
+    }catch(e){
+        errorHandler(res, e);
+    }
 };
 
